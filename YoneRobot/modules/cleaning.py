1f00ad9b70342e2e25219b3c27106e4923f6d164
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-09 23:30:14 +0700

    Create cleaning.py

---
+++ YoneRobot/modules/cleaning.py
@@ -0,0 +1,13 @@
+__mod_name__ = "Cleaning"
+
+__help__ = """
+*Blue text cleaner removed any made up commands that people send in your chat.*
+ ✪ /cleanblue <on/off/yes/no>: clean commands after sending
+ ✪ /ignoreblue <word>: prevent auto cleaning of the command
+ ✪ /unignoreblue <word>: remove prevent auto cleaning of the command
+ ✪ /listblue: list currently whitelisted commands
+ 
+*Following are Disasters only commands, admins cannot use these:*
+ ✪ /gignoreblue <word>: globally ignores bluetext cleaning of saved word across Suzuya.
+ ✪ /ungignoreblue <word>: remove said command from global cleaning list
+"""
