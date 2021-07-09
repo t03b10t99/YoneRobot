02ca0afc664b795ac15c8424348fecb6429129ad
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-09 20:26:33 +0700

    Update gtranslator.py

--- YoneRobot/modules/gtranslator.py
+++ YoneRobot/modules/gtranslator.py
@@ -1,5 +1,5 @@
 from emoji import UNICODE_EMOJI
-from google_trans_new import LANGUAGES, google_translator
+from import LANGUAGES, google_translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
