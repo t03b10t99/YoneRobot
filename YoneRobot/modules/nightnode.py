b15cbfc9faf5ce324b42e211fecbd158fd1ab493
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-09 22:57:52 +0700

    Create nightnode.py

---
+++ YoneRobot/modules/nightnode.py
@@ -0,0 +1,8 @@
+__mod_name__ = "NightMode"
+
+__help__ = """
+ ❍ /addnt: Adds Group to NightMode Chats
+ ❍ /rmnt: Removes Group From NightMode Chats
+ 
+Note: Night Mode chats get Automatically closed at 12pm(IST)
+"""
