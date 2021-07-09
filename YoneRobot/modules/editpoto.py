100b76f60b5b61392054e656d0a86b80d19f31d2
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-09 20:47:08 +0700

    Update editpoto.py

--- YoneRobot/modules/editpoto.py
+++ YoneRobot/modules/editpoto.py
@@ -77,7 +77,7 @@ async def photo(client: Client, message: Message):
     try:
         await client.send_message(
             chat_id=message.chat.id,
-            text="Select your required mode from below!\nCuma mw bilang @tofik_dn ganteng :v",
+            text="Select your required mode from below!",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
