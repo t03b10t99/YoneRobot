8aa720478753fc77385de96c7033fa07aea03d37
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-09 19:03:12 +0700

    Update __telegraph.py

--- YoneRobot/modules/__telegraph.py
+++ YoneRobot/modules/__telegraph.py
@@ -1,18 +1,18 @@
-from YoneRobot.events import register
-from YoneRobot import telethn as tbot
-TMP_DOWNLOAD_DIRECTORY = "./"
-from telethon import events
 import os
 from PIL import Image
+from telethon import events
 from datetime import datetime
+from YoneRobot import telethn as tedebot
+from YoneRobot.events import register
 from telegraph import Telegraph, upload_file, exceptions
-kittu = "YONE"
+td = "TEDE"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=kittu)
+r = telegraph.create_account(short_name=td)
 auth_url = r["auth_url"]
 
+TMP_DOWNLOAD_DIRECTORY = "./"
 
-@register(pattern="^/t(xm|xt) ?(.*)")
+@register(pattern="^/t(m|xt) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -22,7 +22,7 @@ async def _(event):
         r_message = await event.get_reply_message()
         input_str = event.pattern_match.group(1)
         if input_str == "m":
-            downloaded_file_name = await tbot.download_media(
+            downloaded_file_name = await tedebot.download_media(
                 r_message,
                 TMP_DOWNLOAD_DIRECTORY
             )
@@ -42,8 +42,8 @@ async def _(event):
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
                 await h.edit("Uploaded to https://telegra.ph{})".format(media_urls[0]), link_preview=True)
-        elif input_str == "t":
-            user_object = await tbot.get_entity(r_message.sender_id)
+        elif input_str == "xt":
+            user_object = await tedebot.get_entity(r_message.sender_id)
             title_of_page = user_object.first_name # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
             if optional_title:
@@ -52,7 +52,7 @@ async def _(event):
             if r_message.media:
                 if page_content != "":
                     title_of_page = page_content
-                downloaded_file_name = await tbot.download_media(
+                downloaded_file_name = await tedebot.download_media(
                     r_message,
                     TMP_DOWNLOAD_DIRECTORY
                 )
@@ -77,3 +77,12 @@ async def _(event):
 def resize_image(image):
     im = Image.open(image)
     im.save(image, "PNG")
+
+
+__help__ = """
+I can upload files to Telegraph
+ ❍ /tm: Get Telegraph Link Of Replied Media
+ ❍ /txt: Get Telegraph Link of Replied Text
+"""
+
+__mod_name__ = "Telegraph"
